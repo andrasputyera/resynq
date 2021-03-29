dcad844a16df945df14a1c6d242caac8b3ec72dd
andrasputyera <andras.putyera@gmail.com>
2021-03-29 22:29:22 +0200

    created composables and login view

---
+++ src/composables/useSignup.js
@@ -0,0 +1,33 @@
+import { ref } from '@vue/reactivity'
+import { projectAuth } from '../firebase/config'
+
+const error = ref(null)
+const isPending = ref(false)
+
+const signup = async (email, password, displayName) => {
+  error.value = null
+  isPending.value = true
+
+  try {
+    const response = await projectAuth.createUserWithEmailAndPassword(email, password)
+    if (!response) {
+      throw new Error('Could not complete signup')
+    }
+    await response.user.updateProfile({ displayName })
+    error.value = null
+    isPending.value = false
+    
+    return response
+  }
+  catch(err) {
+    console.log(err.message)
+    error.value = err.message
+    isPending.value = false
+  }
+}
+
+const useSignup = () => {
+  return { error, signup, isPending }
+}
+
+export default useSignup
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/auth/Login.vue'
 
 const routes = [
   {
@@ -7,10 +8,14 @@ const routes = [
     name: 'Home',
     component: Home
   },
-  // {
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  
     
   //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
 ]
 
 const router = createRouter({
